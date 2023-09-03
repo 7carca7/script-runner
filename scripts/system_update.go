293865e5f9d8f6c1939f1c31b7fb9785d7a392e7
Ernesto Alejandro <eacarcasses@gmail.com>
2023-09-03 13:50:36 +0200

    system update script completed

---
+++ scripts/system_update.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	// Commands List
+	commands := []string{
+		"brew update",
+		"brew upgrade",
+		"brew autoremove",
+		"brew cleanup",
+		"mas upgrade",
+		"softwareupdate -ia --verbose",
+	}
+
+	for _, command := range commands {
+		fmt.Printf("Executing: %s\n", command)
+
+		// Split the command string into command and arguments
+		parts := strings.Fields(command)
+
+		cmd := exec.Command(parts[0], parts[1:]...)
+
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Command '%s' failed with error: %s\n", command, err)
+			continue
+		}
+
+		/*
+			    Verbose output
+			    output, err := cmd.CombinedOutput()
+					if err != nil {
+						fmt.Printf("Command '%s' failed with error: %s\n", command, err)
+						continue
+					}
+
+					fmt.Printf("%s", output)
+		*/
+	}
+}
